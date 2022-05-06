10f3469453f184f59cbcc9cf839f680035a943ea
DanielBento <dsbld2@gmail.com>
2022-05-06 15:00:01 -0300

    feat: criando funcionalidade de envio de emails

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,20 @@
 import express from 'express'
+import nodemailer from 'nodemailer'
 import { prisma } from './prisma'
 
 const app = express()
 const port = 3333
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "ba24e3424e3888",
+      pass: "9485783217d745"
+    }
+  });
 
 app.post('/feedbacks', async (req,res)=>{
     const {type, comment, screenshot} = req.body;
@@ -15,6 +25,18 @@ app.post('/feedbacks', async (req,res)=>{
             comment,
             screenshot,
         }
+    })
+
+    await transport.sendMail({
+      from: 'Equipe feedget <oi@feedget.com>',
+      to:'Daniel Bento <dsbld2@gmail.com>',
+      subject: 'Novo feedback',
+      html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color:#222;">`,
+        `<p>Tipo do feedback:${type}</p>`,
+        `<p>Comentario:${comment}</p>`,
+        `</div>`
+      ].join('\n')
     })
 
     return res.status(201).json({ data: feedback})
