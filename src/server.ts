fc6afb691cad74c342d169a374e2a014622d6efa
DanielBento <dsbld2@gmail.com>
2022-05-06 14:12:57 -0300

    feat: criação da rota de feedback

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
 import express from 'express'
+import { prisma } from './prisma'
 
 const app = express()
 const port = 3333
+
+app.use(express.json())
+
+app.post('/feedbacks', async (req,res)=>{
+    const {type, comment, screenshot} = req.body;
+
+   const feedback = await prisma.feedback.create({
+        data:{
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    return res.status(201).json({ data: feedback})
+})
 
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`
